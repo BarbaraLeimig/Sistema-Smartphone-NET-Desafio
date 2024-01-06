f3853b333a3e3719155fd8efd2339b27f8196d35
BarbaraLeimig <barbaraleimigdev@gmail.com>
2024-01-06 00:19:05 -0300

    testes no arquivo Program.cs ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Instaciando o objeto nokia a partir da classe Nokia
+Nokia nokia = new Nokia(numero: "+55 81 98765 - 4321", modelo: "Nokia C01 Plus", imei: "123456789012345", memoria: 64);
+nokia.InstalarAplicativo("Instagram");
+
+// Instaciando o objeto iphone a partir da classe Iphone
+Iphone iphone = new Iphone(numero: "+55 81 98679 - 1234", modelo: "Iphone 15", imei: "987654321098765", memoria: 256);
+iphone.InstalarAplicativo("Shopee");
